656a94f393de330f61600462300516a35efa501a
Jinqiang Zeng <jinqiang.zeng@gmail.com>
2021-10-23 20:02:13 -0700

    Added react frontend static server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import { config } from "dotenv";
 import express, { json, urlencoded } from "express";
+import path from "path";
 
 config();
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/restapi", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/", "index.html"));
 });
 
 const port = process.env.PORT || 3001;
